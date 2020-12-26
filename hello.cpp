a454ba846d084e60616c9debb2d6050b28d6a93d
Alex0x45 <alex0x45@gmail.com>
2020-12-25 20:24:13 -0500

    Use auto for types

--- hello.cpp
+++ hello.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
-#include <vector>
-#include <string>
 
 int main()
 {
-    std::vector<std::string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+    auto msg = {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
 
-    for (const std::string& word : msg)
+    for (const auto& word : msg)
     {
         std::cout << word << " ";
     }
